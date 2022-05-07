c8e709922fe4cc2d673f1e72650083a569bcea62
wangyesong <550526434@QQ.COM>
2022-05-07 10:49:38 +0800

    新增

--- WpfApp3/Class1.cs
+++ WpfApp3/Class1.cs
@@ -1,12 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace WpfApp3
+public class Class1
 {
-    class Class1
-    {
-    }
+	public Class1()
+	{
+	}
 }
